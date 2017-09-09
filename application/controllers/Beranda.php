0a571580b7721900abf057532459df54e2823f2a
masbott <masbott@gmail.com>
2017-09-09 23:55:09 +0700

    beranda masbott

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -12,7 +12,7 @@ class Beranda extends CI_Controller
 	}
 
 	function index() {
-		//testing Angga
+		//testing masbott
 		$this->data['sub'] = ['title' => __CLASS__ , 'sub_title' => 'Dashboard Nggaji'];
 		$this->load->view( 'layout/main' , $this->data );
 	}
