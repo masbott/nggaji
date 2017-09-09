df6eca4f2f413c845f07be3b57d79faa0eb62cfc
masbott <masbott@gmail.com>
2017-09-09 23:35:14 +0700

    gitignore

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -20,43 +20,60 @@
                 <a href="<?php echo site_url('beranda') ?>"><i class="fa fa-home"></i> <span class="nav-label">Beranda</span> </a>
             </li>
 
-            <li>
-                <a href="<?php echo site_url('gaji/index') ?>"><i class="fa fa-money"></i> 
-                    <span class="nav-label">Gaji</span>
-                    <span class="fa arrow"></span>
-                </a>
-                <ul class="nav nav-second-level">
-                    <li class="">
-                        <a href="<?php echo site_url('gaji/index') ?>">Hitung</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo site_url('gaji/daftar') ?>">Daftar</a>
-                    </li>
-                </ul>
 
-            </li>
+            <?php if( $this->session->userdata('level') == '1' ): ?>
+                <li>
+                    <a href="<?php echo site_url('gaji/index') ?>"><i class="fa fa-money"></i> 
+                        <span class="nav-label">Gaji</span>
+                        <span class="fa arrow"></span>
+                    </a>
+                    <ul class="nav nav-second-level">
+                        <li class="">
+                            <a href="<?php echo site_url('gaji/index') ?>">Hitung</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo site_url('gaji/daftar') ?>">Daftar</a>
+                        </li>
+                    </ul>
+                </li>
 
-            <li <?php if( $this->uri->segment(1) == 'bonus' ): echo 'class="active"'; endif; ?>>
-                <a href=""><i class="fa fa-diamond"></i> <span class="nav-label">Bonus</span> <span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li class="<?php if( $this->uri->segment(1) == 'bonus' ): echo 'class="active"'; endif; ?>">
-                        <a href="<?php echo site_url('bonus') ?>">Hitung</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo site_url('bonus/daftar') ?>">Daftar</a>
-                    </li>
-                </ul>
-            </li>
-
-            <li>
+                <li>
                 <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Master Data</span> <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li><a href="<?php echo site_url('master_data/jabatan') ?>">Jabatan</a></li>
                     <li><a href="<?php echo site_url('master_data/karyawan') ?>">Karyawan</a></li>
-                    <li><a href="<?php echo site_url('master_data/kriteria') ?>">Kriteria</a></li>
-                    <li><a href="<?php echo site_url('master_data/nilai_kriteria') ?>">Nilai Kriteria</a></li>
+                    <li><a href="">Tunjangan</a></li>
+                    <li><a href="">Potongan</a></li>
                 </ul>
             </li>
+
+            <?php endif; ?>
+            <?php if( $this->session->userdata('level') == '2' ): ?>
+                <li <?php if( $this->uri->segment(1) == 'bonus' ): echo 'class="active"'; endif; ?>>
+                    <a href=""><i class="fa fa-diamond"></i> <span class="nav-label">Bonus</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li class="<?php if( $this->uri->segment(1) == 'bonus' ): echo 'class="active"'; endif; ?>">
+                            <a href="<?php echo site_url('bonus') ?>">Hitung</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo site_url('bonus/daftar') ?>">Daftar</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="<?php echo site_url('gaji/index') ?>"><i class="fa fa-money"></i> 
+                        <span class="nav-label">Gaji</span>
+                        <span class="fa arrow"></span>
+                    </a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="<?php echo site_url('gaji/daftar') ?>">Daftar</a>
+                        </li>
+                    </ul>
+                </li>
+
+            <?php endif; ?>
         </ul>
     </div>
 </nav>
